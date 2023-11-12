5ffa9cd421a7a727e527dd326ba0b7273713fb4d
HasanTorman <torman.hassan@gmail.com>
2023-11-12 17:41:57 +0300

    feat : Create data file and add required data

---
+++ src/data.js
@@ -0,0 +1,94 @@
+export const sidebarData = [
+  {
+    id: 1,
+    filterBy: 'Teams',
+    filterOptions: ['All', 'team1', 'team2'],
+    isSelected: false,
+  },
+  {
+    id: 2,
+    filterBy: 'Agents',
+    filterOptions: ['All', 'team1', 'team2'],
+    isSelected: false,
+  },
+  {
+    id: 3,
+    filterBy: 'Groups',
+    filterOptions: ['All', 'team1', 'team2'],
+    isSelected: false,
+  },
+  {
+    id: 4,
+    filterBy: 'Moments',
+    filterOptions: ['All', 'team1', 'team2'],
+    isSelected: false,
+  },
+  {
+    id: 5,
+    filterBy: 'Call Duraion',
+    filterOptions: ['All', 'team1', 'team2'],
+    isSelected: false,
+  },
+  {
+    id: 6,
+    filterBy: 'Calls Reviwes',
+    filterOptions: ['All', 'team1', 'team2'],
+    isSelected: false,
+  },
+]
+
+export const callsDetailes = [
+  {
+    id: 1,
+    callTiem: '28 Jan 2021 02:29 PM',
+    duration: '00:31',
+    callerType: 'NA',
+    agent: 'John',
+    team: 'Sales team',
+    to_from: 'jone due',
+    type: 'icon',
+    moments: '1 15',
+    scriptComp: '17%',
+    action: 'reviewed',
+  },
+  {
+    id: 2,
+    callTiem: '.....',
+    duration: '...',
+    callerType: '....',
+    agent: '....',
+    team: '....',
+    to_from: '.....',
+    type: '....',
+    moments: '....',
+    scriptComp: '...',
+    action: '....',
+  },
+  {
+    id: 3,
+    callTiem: 'tesr',
+    duration: '...',
+    callerType: '....',
+    agent: '....',
+    team: '....',
+    to_from: '.....',
+    type: '....',
+    moments: '....',
+    scriptComp: '...',
+    action: '....',
+  },
+]
+
+export const tableHead = [
+  'Call date/time',
+  'Duration (mm:ss)',
+  'Caller Type',
+  'Agent',
+  'Team',
+  'To/From',
+  'Type',
+  'Moments',
+  'Script Compliance',
+  'Action',
+]
+
