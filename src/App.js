6e226f51fb4ac12e0ed76f200f64d3e5b0710736
HasanTorman <torman.hassan@gmail.com>
2023-11-12 17:44:30 +0300

    feat : import Components on app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { useState } from 'react'
+import Sidebar from './components/Sidebar'
+import Main from "./components/Main"
+import { sidebarData } from './data'
+
 function App() {
+  const [filterCalls, setFilterCalls] = useState(sidebarData)
+
+  const sidebarConfig = filterCalls.map((item) => {
+    return <Sidebar key={item.id} {...item} />
+  })
+
   return (
-    <div className="App">
+    <div className="py-20 px-12">
+      <h1 className="mb-2 text-xl font-medium">enthu.ai</h1>
+      <section className="flex">  
+        <Sidebar  {...filterCalls} />
+        <Main/>
+      </section>
     </div>
   )
 }
